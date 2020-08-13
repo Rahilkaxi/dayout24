d598263511cbd4a543845b53a3fb1c037b7ddca5
rahilkazi <rahilkazi66@gmail.com>
2020-08-13 13:19:18 +0530

    make it

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 import django_heroku
 import cloudinary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(verbose=True)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
